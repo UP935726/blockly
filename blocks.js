cd66f771a3b6b9f0f4f7505f5598a58acdbab745
Matt Chris Hawkins <72578727+UP935726@users.noreply.github.com>
2023-02-02 17:12:44 +0000

    Got IF mutator working

--- blocks.js
+++ blocks.js
@@ -1,11 +1,27 @@
 //Try to get mutator to work on if statements 
+//https://groups.google.com/g/blockly/c/sS_UUW8-D-w
+//https://playcode.io/
+
+//Start code block
+Blockly.Blocks['start_code'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Start");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(135);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
 
 //Get the string length
 Blockly.Blocks['string_length'] = {
   init: function() {
     this.appendValueInput('VALUE')
-        .setCheck('String')
-        .appendField('length of');
+        .setCheck('String');
+    this.appendDummyInput()
+        .appendField('.length');
     this.setOutput(true, 'Number');
     this.setColour(60);
     this.setTooltip('Returns number of letters in the provided text.');
@@ -15,10 +31,12 @@ Blockly.Blocks['string_length'] = {
 //Print statement
 Blockly.Blocks['print_statement'] = {
   init: function() {
-    this.appendValueInput("userInput")
-        .setCheck("String")
-        .appendField("print");
-    this.setInputsInline(false);
+    this.appendValueInput("user_input")
+        .setCheck(null)
+        .appendField("window.alert(");
+    this.appendDummyInput()
+        .appendField(");");
+    this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -31,9 +49,9 @@ Blockly.Blocks['print_statement'] = {
 Blockly.Blocks['text'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("\"")
+        .appendField("'")
         .appendField(new Blockly.FieldTextInput("default"), "user_input")
-        .appendField("\"");
+        .appendField("'");
     this.setInputsInline(true);
     this.setOutput(true, "String");
     this.setColour(60);
@@ -76,7 +94,7 @@ Blockly.Blocks['simple_maths'] = {
     this.appendValueInput("first_input")
         .setCheck("Number");
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["+","addition"], ["-","subtraction"], ["*","multiplication"], ["/","division"], ["^","power"]]), "NAME");
+        .appendField(new Blockly.FieldDropdown([["+","addition"], ["-","subtraction"], ["*","multiplication"], ["/","division"], ["**","power"]]), "user_choice");
     this.appendValueInput("second_input")
         .setCheck("Number");
     this.setInputsInline(true);
@@ -88,34 +106,171 @@ Blockly.Blocks['simple_maths'] = {
 };
 
 //If statement
+// Blockly.Blocks['if_statement'] = {
+//   init: function() {
+//     this.appendValueInput("statement_input")
+//         .setCheck("Boolean")
+//         .setAlign(Blockly.ALIGN_RIGHT)
+//         .appendField("If (");
+//     this.appendDummyInput()
+//         .appendField(")");
+//     this.appendStatementInput("statement_output")
+//         .setCheck("statement")
+//     this.setInputsInline(true);
+//     this.setPreviousStatement(true, null);
+//     this.setNextStatement(true, null);
+//     this.setColour(260);
+//  this.setTooltip("");
+//  this.setHelpUrl("");
+//  }
+// };
+
+//Testing mutators
 Blockly.Blocks['if_statement'] = {
   init: function() {
-    this.appendValueInput("statement_input")
+    this.appendValueInput("if0")
         .setCheck("Boolean")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("If (");
     this.appendDummyInput()
         .appendField(")");
-    this.appendStatementInput("statement_output")
+    this.appendStatementInput("do0")
         .setCheck("statement")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("do");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(260);
+    this.setMutator(new Blockly.Mutator(['if_statement_elseif', 'if_statement_else']));
+    this.setNextStatement(true);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  },
+  //Returns the state of this block as a JSON serializable object
+  saveExtraState: function() {
+    if (!this.elseifCount_ && !this.elseCount_) {
+      return null;
+    }
+    const state = Object.create(null);
+    if (this.elseifCount_) {
+      state['elseIfCount'] = this.elseifCount_;
+    }
+    if (this.elseCount_) {
+      state['hasElse'] = true;
+    }
+    return state;
+  },
+  //Applies the given state to this block
+  loadExtraState: function(state) {
+    this.elseifCount_ = state['elseIfCount'] || 0;
+    console.log(state['elseIfCount']);
+    this.elseCount_ = state['hasElse'] ? 1 : 0;
+    this.updateShape_();
+  },
+  //Populate mutator space with components
+  decompose: function(workspace) {
+    const topBlock = workspace.newBlock('if_statement_if');
+    topBlock.initSvg();
+    let connection = topBlock.nextConnection;
+    for (let i = 1; i <= this.elseifCount_; i++) {
+      const elseifBlock = workspace.newBlock('if_statement_elseif');
+      elseifBlock.initSvg();
+      connection.connect(elseifBlock.previousConnection);
+      connection = elseifBlock.nextConnection;
+    }
+    if (this.elseCount_) {
+      const elseBlock = workspace.newBlock('if_statement_else');
+      elseBlock.initSvg();
+      connection.connect(elseBlock.previousConnection);
+    }
+    return topBlock;
+  },
+  //Reconfigure block based on the mutator dialog's components
+  compose:function(topBlock){
+    let clauseBlock = topBlock.nextConnection.targetBlock();
+    //Count number of inputs
+    this.elseifCount_ = 0;
+    this.elseCount_ = 0;
+    const valueConnections = [null];
+    const statementConnections = [null];
+    let elseStatementConnection = null;
+    while (clauseBlock && !clauseBlock.isInsertionMarker()){
+      switch(clauseBlock.type){
+        case 'if_statement_elseif':
+          this.elseifCount_++;
+          valueConnections.push(clauseBlock.valueConnection_);
+          statementConnections.push(clauseBlock.statementConnection_);
+          break;
+        case 'if_statement_else':
+          this.elseCount_++;
+          elseStatementConnection = clauseBlock.statmentConnection_;
+          break;
+        default:
+          throw TypeError('Unknown block type: ' + clauseBlock.type);
+      }
+      clauseBlock = clauseBlock.nextConnection &&
+        clauseBlock.nextConnection.targetBlock();
+    }
+    this.updateShape_();
+  },
+  //Modify block to have the correct number of inputs
+  updateShape_:function(){
+    //Delete everything
+    if(this.getInput('else')){
+      this.removeInput('else');
+    }
+    for(let i = 1; this.getInput('if' + i); i++){
+      this.removeInput('if' + i);
+      this.removeInput('do' + i)
+    }
+    //Rebuild block
+    for(let i = 1; i <= this.elseifCount_; i++){
+      this.appendValueInput("if" + i)
+          .setCheck('Boolean')
+          .appendField('else if ')
+      this.setInputsInline(true);
+      this.appendStatementInput('do' + i)
+          .appendField('');
+    }
+    if(this.elseCount_){
+      this.appendStatementInput('else')
+          .appendField('else ');
+    }
+  }  
+};
+
+Blockly.Blocks['if_statement_if'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("if");
+    this.setInputsInline(true);
+    this.setNextStatement(true, ["elseif", "else"]);
+    this.setColour(260);
  this.setTooltip("");
  this.setHelpUrl("");
   }
-  // saveExtraState: function(){
-  //   return{
-  //     'itemCount' : this.itemCount_,
-  //   }
-  // },
-  // loadExtraState: function(state){
-  //   this.itemCount_ = state['itemCount'];
-  //   this.updateShape_();
-  // }
+};
+
+Blockly.Blocks['if_statement_elseif'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("else if");
+    this.setPreviousStatement(true, ["if", "elseif"]);
+    this.setNextStatement(true, ["elseif", "else"]);
+    this.setColour(260);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['if_statement_else'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("else");
+    this.setPreviousStatement(true, ["elseif", "if"]);
+    this.setColour(260);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
 };
 
 //Block to call a variable
@@ -131,13 +286,28 @@ Blockly.Blocks['variable'] = {
   }
 };
 
+//Block to intialise a variable
+// Blockly.Blocks['var'] = {
+//   init: function() {
+//     this.appendDummyInput()
+//         .appendField("var")
+//         .appendField(new Blockly.FieldVariable(null), "variable_name");
+//     this.setInputsInline(true);
+//     this.setPreviousStatement(true, null);
+//     this.setNextStatement(true, null);
+//     this.setColour(350);
+//  this.setTooltip("");
+//  this.setHelpUrl("");
+//   }
+// };
+
 //Compare two inputs
 Blockly.Blocks['compare'] = {
   init: function() {
     this.appendValueInput("first_input")
         .setCheck(null);
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["=","equal"], ["!=","not_equal"], [">=","greater_equal"], ["<=","lesser_equal"], [">","greater"], ["<","lesser"]]), "comparison");
+        .appendField(new Blockly.FieldDropdown([["==","equal"], ["===","strict_equal"], ["!=","not_equal"], ["!==","strict_not_equal"], [">=","greater_equal"], ["<=","lesser_equal"], [">","greater"], ["<","lesser"]]), "comparison");
     this.appendValueInput("second_input")
         .setCheck(null);
     this.setInputsInline(true);
@@ -160,7 +330,7 @@ Blockly.Blocks['compound_statement'] = {
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("}");
     this.setPreviousStatement(true, null);
-    this.setColour(260);
+    this.setColour(43);
  this.setTooltip("This block allows multiple statements to process as 1 statement.");
  this.setHelpUrl("");
   }
@@ -173,20 +343,40 @@ Blockly.Blocks['variable_getter'] = {
         .appendField(new Blockly.FieldVariable("Default"), "variable_input");
     this.setOutput(true, null);
     this.setColour(350);
+  this.setTooltip("");
+  this.setHelpUrl("");
   }
 };
 
 // Block for variable setter.
 Blockly.Blocks['variable_setter'] = {
   init: function() {
-    this.appendValueInput("VALUE")
+    this.appendValueInput("first_input")
         .setCheck(null)
-        .appendField("set")
-        .appendField(new Blockly.FieldVariable("Default"), "variable_input")
-        .appendField("to");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(350);
+        .appendField(new Blockly.FieldVariable("default"), "variable_input")
+        .appendField("=");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(350);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Let variable equal something
+Blockly.Blocks['let_equal'] = {
+  init: function() {
+    this.appendValueInput("first_input")
+        .setCheck(null)
+        .appendField("let")
+        .appendField(new Blockly.FieldVariable(null), "variable_input")
+        .appendField("=");
+    this.setInputsInline(true);
+    this.setOutput(true, "Statement");
+    this.setColour(350);
+ this.setTooltip("");
+ this.setHelpUrl("");
   }
 };
 
@@ -194,7 +384,7 @@ Blockly.Blocks['variable_setter'] = {
 Blockly.Blocks['boolean_output'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["True","true"], ["False","false"]]), "boolean");
+        .appendField(new Blockly.FieldDropdown([["true","true"], ["false","false"]]), "boolean");
     this.setOutput(true, "Boolean");
     this.setColour(300);
  this.setTooltip("Returns the value true or false");
@@ -206,16 +396,14 @@ Blockly.Blocks['boolean_output'] = {
 Blockly.Blocks['for_loop'] = {
   init: function() {
     this.appendValueInput("first_input")
-        .setCheck("Number")
+        .setCheck("Statement")
         .appendField("for (")
-        .appendField(new Blockly.FieldVariable("i"), "VARIABLE")
-        .appendField("=");
     this.appendValueInput("second_input")
-        .setCheck("Number")
-        .appendField("to");
+        .setCheck("Boolean")
+        .appendField("; ");
     this.appendValueInput("third_input")
-        .setCheck("Number")
-        .appendField("by");
+        .setCheck("Boolean")
+        .appendField("; ");
     this.appendDummyInput()
         .appendField(") {");
     this.appendStatementInput("statement_input")
@@ -290,3 +478,46 @@ Blockly.Blocks['declare_array'] = {
   }
 };
 
+//Convert expression to statement
+Blockly.Blocks['expression_to_statement'] = {
+  init: function() {
+    this.appendValueInput("expression")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(";");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(43);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Increment variable
+Blockly.Blocks['increment'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable(null), "variable_input")
+        .appendField("++");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(300);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Decrement variable
+Blockly.Blocks['decrement'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable(null), "variable_input")
+        .appendField("--");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(300);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
--- generator.js
+++ generator.js
@@ -1,3 +1,9 @@
+//Start code block
+Blockly.JavaScript['start_code'] = function() {
+  var code = '';
+  return code;
+};
+
 //Get the string length
 Blockly.JavaScript['string_length'] = function(block) {
   var value_string_length = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
@@ -7,7 +13,7 @@ Blockly.JavaScript['string_length'] = function(block) {
 
 //Print statement
 Blockly.JavaScript['print_statement'] = function(block) {
-  var value_userinput = Blockly.JavaScript.valueToCode(block, 'userInput', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_userinput = Blockly.JavaScript.valueToCode(block, 'user_input', Blockly.JavaScript.ORDER_ATOMIC);
   // return code;
   return 'window.alert(' + value_userinput + ');\n';
 };
@@ -15,9 +21,8 @@ Blockly.JavaScript['print_statement'] = function(block) {
 //String input from user
 Blockly.JavaScript['text'] = function(block) {
   var text_user_input = block.getFieldValue('user_input');
-  var code = '"'+text_user_input+'"';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = "'" + text_user_input + "'";
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //Convert number to string
@@ -25,97 +30,119 @@ Blockly.JavaScript['num_to_string'] = function(block) {
   var value_value = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = value_value + '.toString()';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //Number input from user
 Blockly.JavaScript['number'] = function(block) {
   var number_value = block.getFieldValue('VALUE');
   var code = number_value;
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //Simple maths function
 Blockly.JavaScript['simple_maths'] = function(block) {
   var value_first_input = Blockly.JavaScript.valueToCode(block, 'first_input', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_name = block.getFieldValue('NAME');
+  var dropdown_user_choice = block.getFieldValue('user_choice');
   var value_second_input = Blockly.JavaScript.valueToCode(block, 'second_input', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = '...';
-  if (dropdown_name == "addition") {
-    code = value_first_input + '+' + value_second_input;
-  } else if (dropdown_name == "subtraction"){
-    code = value_first_input + '-' + value_second_input;
-  } else if (dropdown_name == "multiplication"){
-    code = value_first_input + '*' + value_second_input;
-  } else if (dropdown_name == "division"){
-    code = value_first_input + '/' + value_second_input;
-  } else if (dropdown_name == "power"){
-    code = 'Math.pow(' + value_first_input + ',' + value_second_input + ')';
+  if (dropdown_user_choice == "addition") {
+    code = value_first_input + ' + ' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_ADDITION];
+  } else if (dropdown_user_choice == "subtraction"){
+    code = value_first_input + ' - ' + value_second_input;
+    return [code, Blockly.JavaScript.SUBTRACTION];
+  } else if (dropdown_user_choice == "multiplication"){
+    code = value_first_input + ' * ' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_MULTIPLICATION];
+  } else if (dropdown_user_choice == "division"){
+    code = value_first_input + ' / ' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_DIVISION];
+  } else if (dropdown_user_choice == "power"){
+    code = value_first_input + ' ** ' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_EXPONENTIATION];
   };
-
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 
 //If statement
 Blockly.JavaScript['if_statement'] = function(block) {
-  var code_to_test = Blockly.JavaScript.valueToCode(block, 'statement_input', Blockly.JavaScript.ORDER_ATOMIC);
-  var statement = Blockly.JavaScript.statementToCode(block, 'statement_output');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'if(' + code_to_test +'){\n' + statement + '};\n';
-  return code;
+  let n = 0;
+  let code = '';
+  do {
+    const conditionCode = Blockly.JavaScript.valueToCode(block, 'if' + n, Blockly.JavaScript.ORDER_NONE) || 'false';
+    let branchCode = Blockly.JavaScript.statementToCode(block, 'do' + n);
+    code += (n > 0 ? ' else ' : '') + 'if (' + conditionCode + ') {\n' + branchCode + '}';
+    n++;
+  } while (block.getInput('if' + n));
+
+  if (block.getInput('else')) {
+    let branchCode = Blockly.JavaScript.statementToCode(block, 'else');
+    code += ' else {\n' + branchCode + '}';
+  }
+  return code + '\n';
 };
 
+
 //Block to call a variable
+
+//Block to initialise a variable
+// Blockly.JavaScript['var'] = function(block) {
+//   var variable_name = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('variable_name'));
+//   // TODO: Assemble JavaScript into code variable.
+//   var code = 'var ' + variable_name + ';\n';
+//   return code;
+// };
 
 //Compare two inputs
 Blockly.JavaScript['compare'] = function(block) {
   var value_first_input = Blockly.JavaScript.valueToCode(block, 'first_input', Blockly.JavaScript.ORDER_ATOMIC);
   var dropdown_comparison = block.getFieldValue('comparison');
   var value_second_input = Blockly.JavaScript.valueToCode(block, 'second_input', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
   var code = '...';
   if (dropdown_comparison == "equal") {
     code = value_first_input + '==' + value_second_input;
-  } else if (dropdown_comparison == "doesnt_equal"){
+    return [code, Blockly.JavaScript.ORDER_EQUALITY];
+  } else if (dropdown_comparison == "not_equal"){
     code = value_first_input + '!=' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_EQUALITY];
+  } else if (dropdown_comparison == "strict_equal"){
+    code = value_first_input + '===' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_EQUALITY];
+  } else if (dropdown_comparison == "strict_not_equal"){
+    code = value_first_input + '!==' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_EQUALITY];
   } else if (dropdown_comparison == "greater_equal"){
     code = value_first_input + '>=' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_RELATIONAL];
   } else if (dropdown_comparison == "lesser_equal"){
     code = value_first_input + '<=' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_RELATIONAL];
   } else if (dropdown_comparison == "greater"){
     code = value_first_input + '>' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_RELATIONAL];
   } else if (dropdown_comparison == "lesser"){
     code = value_first_input + '<' + value_second_input;
+    return [code, Blockly.JavaScript.ORDER_RELATIONAL];
   };
-
-
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
 };
-
 
 //Compound statement block
 Blockly.JavaScript['compound_statement'] = function(block) {
   var statements_compound = Blockly.JavaScript.statementToCode(block, 'compound');
-  // TODO: Assemble JavaScript into code variable.
   var code = '{\n\t' + statements_compound + '\n};';
   return code;
 };
 
-// Block for variable getter.
+//Variable block
 Blockly.JavaScript['variable_getter'] = function(block) {
   var value_variable = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('variable_input'), Blockly.Names.NameType.VARIABLE);
   var code = value_variable;
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
-// Block for variable setter.
+//Assigning a varaible a value
 Blockly.JavaScript['variable_setter'] = function(block) {
   var value_variable = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('variable_input'), Blockly.Names.NameType.VARIABLE);
   var value_setting = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
@@ -123,58 +150,48 @@ Blockly.JavaScript['variable_setter'] = function(block) {
   return code;
 };
 
+//Let variable equal something
+Blockly.JavaScript['let_equal'] = function(block) {
+  var value_variable = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('variable_input'), Blockly.Names.NameType.VARIABLE);
+  var first_input = Blockly.JavaScript.valueToCode(block, 'first_input', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'let ' + value_variable + ' = ' + first_input;
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
 //True or false
 Blockly.JavaScript['boolean_output'] = function(block) {
   var dropdown_boolean = block.getFieldValue('boolean');
-  // TODO: Assemble JavaScript into code variable.
   var code = '...';
   if (dropdown_boolean == "true") {
     code = 'true';
   } else if (dropdown_boolean == "false"){
     code = 'false';
   }
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //For loop
 Blockly.JavaScript['for_loop'] = function(block) {
-  var variable_input = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('VARIABLE'), Blockly.Names.NameType.VARIABLE);
   var value_first_input = Blockly.JavaScript.valueToCode(block, 'first_input', Blockly.JavaScript.ORDER_ATOMIC);
   var value_second_input = Blockly.JavaScript.valueToCode(block, 'second_input', Blockly.JavaScript.ORDER_ATOMIC);
   var value_third_input = Blockly.JavaScript.valueToCode(block, 'third_input', Blockly.JavaScript.ORDER_ATOMIC);
   var statement_input = Blockly.JavaScript.statementToCode(block, 'statement_input');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'for (' + variable_input + ' = ' + value_first_input + '; ' + variable_input + ' < ' + value_second_input + '; '
-              + variable_input + ' += ' + value_third_input + '){\n' + statement_input + '};\n';
+  var code = 'for (' + value_first_input + '; ' + value_second_input + '; '
+              + value_third_input + '){\n' + statement_input + '};\n';
   return code;
 };
 
-//Example for loop code
-// for (let i = 0; i < cars.length; i++) {
-//   text += cars[i] + "<br>";
-// }
-
 //While loop
-
 Blockly.JavaScript['while_loop'] = function(block) {
   var value_first_input = Blockly.JavaScript.valueToCode(block, 'first_input', Blockly.JavaScript.ORDER_ATOMIC);
   var statement_input = Blockly.JavaScript.statementToCode(block, 'statement_input');
-  // TODO: Assemble JavaScript into code variable.
   var code = 'while (' + value_first_input + '){\n' + statement_input + '};\n';
   return code;
 };
 
-//example while loop code
-// while (i < 10) {
-//   text += "The number is " + i;
-//   i++;
-// }
-
 //Break or continue
 Blockly.JavaScript['break'] = function(block) {
   var dropdown_value = block.getFieldValue('first_input');
-  // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   if (dropdown_value == "breakout") {
     code = 'break;\n';
@@ -190,7 +207,6 @@ Blockly.JavaScript['declare_array'] = function(block) {
   var type_choice = block.getFieldValue('type_choice');
   var dimensions = block.getFieldValue('dimensions');
   var num_of_items = block.getFieldValue('num_of_items');
-  // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   var code = variable + ' = ' + 'new Array(' + dimensions + ');\n'
   if (type_choice == "number"){
@@ -203,3 +219,23 @@ Blockly.JavaScript['declare_array'] = function(block) {
   return code;
 };
 
+//Convert expression to statement
+Blockly.JavaScript['expression_to_statement'] = function(block) {
+  var value_expression= Blockly.JavaScript.valueToCode(block, 'expression', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = value_expression + ';\n';
+  return code;
+};
+
+//Increment variable 
+Blockly.JavaScript['increment'] = function(block) {
+  var variable_input = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('variable_input'), Blockly.Names.NameType.VARIABLE);
+  var code = variable_input + '++';
+  return [code, Blockly.JavaScript.ORDER_INCREMENT];
+};
+
+//Decrement variable 
+Blockly.JavaScript['decrement'] = function(block) {
+  var variable_input = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('variable_input'), Blockly.Names.NameType.VARIABLE);
+  var code = variable_input + '--';
+  return [code, Blockly.JavaScript.ORDER_DECREMENT];
+};
